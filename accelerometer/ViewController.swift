1162146b491c57c3004ff0a8214fab5a50335767
maoko <maaaoko0021@gmail.com>
2022-08-26 13:59:25 +0900

    Initial Commit

---
+++ accelerometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  accelerometer
+//
+//  Created by Maoko Furuya on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
